70fc4152180d51bc818bf2550edb3163f8df3e14
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-03-21 01:11:38 +0400

    .........

---
+++ Ado.Net/Ado.Net/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ado.Net
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sql=null;
+            string connectionString;
+            connectionString= @"Data Source= localhost\SQLEXPRESS;Initial Catalog=University;User ID=UserName;Password=Password";
+            sql = new SqlConnection(connectionString);
+            try
+            {
+                sql.Open();
+                MessageBox.Show("Connection open!");
+                sql.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Can not open connection !");
+            }
+        }
+    }
+}
